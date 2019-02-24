59c6d27c14453ff04945eaf1f8814cf9217891ae
ahcuanya <achuan@achuan.io>
2019-02-24 22:18:19 +0800

    修改了项目配置文件

--- app/config.php
+++ app/config.php
@@ -1,16 +1,17 @@
 <?php
 
-/**
- * 入口文件(index.php)中的 $config 变量收到配置参数后, 再传给框架的核心类(ac.php)
- * @author 阿川 <ahuan@achuan.io>
- * 2019-02-21 19:33
+/*
+ * @Description: 项目配置文件 
+ * @author: 阿川 ahuan@achuan.io
+ * @Date: 2019-02-21 19:01:01
+ * @LastEditTime: 2019-02-24 16:42:42
  */
 
 // 数据库配置信息
-$config['DNS_DB']['host'] = 'localhost';
-$config['USER_DB']['username'] = 'achuan';
-$config['PWD_DB']['password'] = '200529';
-$config['PJT_DB']['dbname'] = 'project';
+$config['db']['host'] = 'localhost';
+$config['db']['username'] = 'achuan';
+$config['db']['password'] = '200529';
+$config['db']['dbname'] = 'project';
 
 // 默认的控制器和操作名
 $config['defaultController'] = "Item";
