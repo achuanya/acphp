cdaf47b4e47a8f9b7f69a0de4bbd97d105b23459
achuanya <achuan@achuan.io>
2019-04-05 19:20:09 +0800

    增加连接到数据库

--- core/db/Db.php
+++ core/db/Db.php
@@ -13,5 +13,19 @@ use PDOException;
  * Class Db
  */
 class Db {
-    
+    private static $pdo = null;
+
+    public static function pdo() {
+        if (self::$pdo !== null) {
+            return self::$pdo;
+        }
+        try {
+            $dsn = sprintf('mysql:host=%s; dbname=%s; charset=utf8', DB_HOST, DB_NAME);
+            $option = array(PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC);
+
+            return self::$pdo = new PDO($dsn, DB_USER, DB_PASS, $option);
+        } catch (PDOException $e) {
+            exit($e->getMessage());
+        }
+    }
 }
