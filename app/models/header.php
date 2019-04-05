04b323170047b2effacf37d71236e60a2a257c2b
achuanya <achuan@achuan.io>
2019-04-05 22:04:17 +0800

    新增header视图

--- app/models/header.php
+++ app/models/header.php
@@ -1,6 +1,13 @@
-<?php
-/**
- * Created by PhpStorm.
- * @author: 阿川 <achuan@achuan.io>
- * @Time: 2019/4/5 21:34
- */
+<!doctype html>
+<html lang="en">
+<head>
+    <base href="/<?=APP_NAME;?>/">
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title><?php echo $title ?></title>
+    <link rel="stylesheet" href="./css/main.css" type="text/css">
+</head>
+<body>
+<h1><?php echo $title ?></h1>
